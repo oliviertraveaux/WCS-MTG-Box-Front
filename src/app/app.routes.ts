616cd4291c6f6c151970d7825c32ea9d7eb8474f
Olivier Traveaux <olivier.traveaux@gmail.com>
2024-01-15 20:00:40 +0100

    basic navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {SearchPageComponent} from "./features/search/components/ui/search-page/search-page.component";
+import {CollectionPageComponent} from "./features/collection/components/ui/collection-page/collection-page.component";
+import {ProfilPageComponent} from "./features/profil/components/ui/profil-page/profil-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchPageComponent },
+  { path: 'collection', component: CollectionPageComponent },
+  { path: 'profil', component: ProfilPageComponent },
+];
---
+++ src/app/features/profil/components/ui/profil-page/profil-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profil-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profil-page.component.html',
+  styleUrls: ['./profil-page.component.scss']
+})
+export class ProfilPageComponent {
+
+}
