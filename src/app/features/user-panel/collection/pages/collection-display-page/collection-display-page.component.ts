4dedb579a59bf6503b370e061f77e0fb1a0501a6
RTimmerGH <timmer.romain@gmail.com>
2024-03-19 16:25:33 +0100

    feat - creation collection display page

---
+++ src/app/features/user-panel/collection/pages/collection-display-page/collection-display-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-collection-display-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './collection-display-page.component.html',
+  styleUrls: ['./collection-display-page.component.scss']
+})
+export class CollectionDisplayPageComponent {
+
+}
