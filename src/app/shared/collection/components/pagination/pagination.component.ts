f8230dc7281771512d1cbec548bf5e5a0d0d50e2
RTimmerGH <timmer.romain@gmail.com>
2024-04-15 18:06:26 +0200

    fix - display list and card basket sort table + fix pagination

--- src/app/shared/collection/components/pagination/pagination.component.ts
+++ src/app/shared/collection/components/pagination/pagination.component.ts
@@ -1,29 +1,24 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatPaginatorModule, PageEvent} from "@angular/material/paginator";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
-  selector: 'app-pagination',
-  standalone: true,
-  imports: [CommonModule, MatPaginatorModule],
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+    selector: 'app-pagination',
+    standalone: true,
+    imports: [CommonModule, MatPaginatorModule],
+    templateUrl: './pagination.component.html',
+    styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent {
+    @Input() length: number = 0;
+    @Input() pageSize: number = 10;
+    @Input() pageIndex: number = 0;
+    @Input() pageSizeOptions: number[] = [10, 20, 50];
+    @Output() page = new EventEmitter<{ startIndex: number; endIndex: number }>();
 
-  @Input() length : number = 0;
-  @Input() pageSize : number = 10;
-  @Input() pageIndex : number = 0;
-  @Input() pageSizeOptions: number[] = [10, 20, 50];
-  @Output() page = new EventEmitter<{ startIndex: number; endIndex: number }>();
-
-
-
-
-  handlePageEvent(event: PageEvent) {
-    const startIndex = event.pageIndex * event.pageSize;
-    const endIndex = startIndex + event.pageSize;
-    this.page.emit({ startIndex, endIndex });
-  }
+    handlePageEvent(event: PageEvent) {
+        const startIndex = event.pageIndex * event.pageSize;
+        const endIndex = startIndex + event.pageSize;
+        this.page.emit({ startIndex, endIndex });
+    }
 }
-
