b7ce24c3915b65631bf12c878f137469c842d8ad
Antoine <cassagnea@gmail.com>
2024-06-10 12:21:39 +0200

    Add prodile

--- src/app/features/auth/pages/create-account/create-account.component.ts
+++ src/app/features/auth/pages/create-account/create-account.component.ts
@@ -1,208 +1,209 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {
-    AbstractControl,
-    AsyncValidatorFn,
-    FormBuilder,
-    FormsModule,
-    ReactiveFormsModule,
-    ValidationErrors,
-    Validators,
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
 } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatStepperModule } from '@angular/material/stepper';
-import { Router } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { Observable, map, of } from 'rxjs';
-import { SnackbarStatus } from '../../../../shared/enums/snackbar-status.enum';
-import { AlertService } from '../../../../shared/services/alert.service';
-import { RegistrationFormData } from '../../models/auth.model';
-import { francePostalCodes } from '../../shared/postal-code-utils';
-import { RegisterService } from '../../shared/services/register.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatStepperModule} from '@angular/material/stepper';
+import {Router} from '@angular/router';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {Observable, map, of} from 'rxjs';
+import {SnackbarStatus} from '../../../../shared/enums/snackbar-status.enum';
+import {AlertService} from '../../../../shared/services/alert.service';
+import {RegisterService} from '../../shared/services/register.service';
+import {franceDepartments} from "../../shared/departments-utils";
+
+
 
 @Component({
-    selector: 'app-create-account',
-    standalone: true,
-    imports: [
-        CommonModule,
-        MatStepperModule,
-        MatInputModule,
-        MatSelectModule,
-        MatButtonModule,
+  selector: 'app-create-account',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
 
-        FormsModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSnackBarModule,
-        MatIconModule,
-        TranslateModule,
-    ],
-    templateUrl: './create-account.component.html',
-    styleUrls: ['./create-account.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatIconModule,
+    TranslateModule,
+  ],
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateAccountComponent {
-    private _alertService = inject(AlertService);
-    private _formBuilder = inject(FormBuilder);
-    private _registerService = inject(RegisterService);
-    private _router = inject(Router);
+  private _alertService = inject(AlertService);
+  private _formBuilder = inject(FormBuilder);
+  private _registerService = inject(RegisterService);
+  private _router = inject(Router);
 
-    private _translate = inject(TranslateService);
+  private _translate = inject(TranslateService);
 
-    isLinear = false;
-    isConfirmPasswordHidden = true;
-    isUsernameAvailable: boolean = true;
-    isEmailAvailable: boolean = true;
-    departments: any = francePostalCodes;
+  isLinear = false;
+  isConfirmPasswordHidden = true;
+  isUsernameAvailable: boolean = true;
+  isEmailAvailable: boolean = true;
+  departments: any = franceDepartments;
 
-    userNameFormGroup = this._formBuilder.group({
-        userName: [
-            '',
-            {
-                validators: [
-                    Validators.required,
-                    Validators.minLength(3),
-                    Validators.maxLength(50),
-                ],
-                asyncValidators: [
-                    this.checkUsernameAvailabilityValidator(
-                        this._registerService,
-                        this._alertService
-                    ),
-                ],
-                updateOn: 'blur',
-            },
+  userNameFormGroup = this._formBuilder.group({
+    userName: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50),
         ],
-    });
-    emailFormGroup = this._formBuilder.group({
-        email: [
-            '',
-            {
-                validators: [Validators.required, Validators.email],
-                asyncValidators: [
-                    this.checkEmailAvailabilityValidator(this._registerService, this._alertService),
-                ],
-                updateOn: 'blur',
-            },
+        asyncValidators: [
+          this.checkUsernameAvailabilityValidator(
+            this._registerService,
+            this._alertService
+          ),
         ],
-    });
-    passwordFormGroup = this._formBuilder.group({
-        password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-    confirmPasswordFormGroup = this._formBuilder.group({
-        confirmPassword: ['', Validators.required],
-    });
-    postCodeFormGroup = this._formBuilder.group({
-        postCode: ['', [Validators.required]],
-    });
-    cityFormGroup = this._formBuilder.group({
-        city: ['', Validators.required],
-    });
+        updateOn: 'blur',
+      },
+    ],
+  });
+  emailFormGroup = this._formBuilder.group({
+    email: [
+      '',
+      {
+        validators: [Validators.required, Validators.email],
+        asyncValidators: [
+          this.checkEmailAvailabilityValidator(this._registerService, this._alertService),
+        ],
+        updateOn: 'blur',
+      },
+    ],
+  });
+  passwordFormGroup = this._formBuilder.group({
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  confirmPasswordFormGroup = this._formBuilder.group({
+    confirmPassword: ['', Validators.required],
+  });
+  departmentFormGroup = this._formBuilder.group({
+    department: ['', [Validators.required]],
+  });
+  cityFormGroup = this._formBuilder.group({
+    city: ['', Validators.required],
+  });
 
-    onSubmit() {
-        if (
-            this.userNameFormGroup.valid &&
-            this.emailFormGroup.valid &&
-            this.passwordFormGroup.valid &&
-            this.confirmPasswordFormGroup.valid &&
-            this.postCodeFormGroup.valid &&
-            this.cityFormGroup.valid &&
-            this.isUsernameAvailable &&
-            this.isEmailAvailable
-        ) {
-            if (
-                this.passwordFormGroup.value.password !==
-                this.confirmPasswordFormGroup.value.confirmPassword
-            ) {
-                const passwordMismatch = this._translate.instant('Toasts.passwords-match');
-                this._alertService.openSnackBar(passwordMismatch, SnackbarStatus.error);
+  onSubmit() {
+    if (
+      this.userNameFormGroup.valid &&
+      this.emailFormGroup.valid &&
+      this.passwordFormGroup.valid &&
+      this.confirmPasswordFormGroup.valid &&
+      this.departmentFormGroup.valid &&
+      this.cityFormGroup.valid &&
+      this.isUsernameAvailable &&
+      this.isEmailAvailable
+    ) {
+      if (
+        this.passwordFormGroup.value.password !==
+        this.confirmPasswordFormGroup.value.confirmPassword
+      ) {
+        const passwordMismatch = this._translate.instant('Toasts.passwords-match');
+        this._alertService.openSnackBar(passwordMismatch, SnackbarStatus.error);
 
-                return;
-            }
-            this.performRegistration();
-        }
+        return;
+      }
+      this.performRegistration();
     }
+  }
 
-    performRegistration() {
-        const formData: RegistrationFormData = {
-            username: this.userNameFormGroup.get('userName')?.value as string,
-            email: this.emailFormGroup.get('email')?.value as string,
-            password: this.passwordFormGroup.get('password')?.value as string,
-            postCode: parseInt(this.postCodeFormGroup.get('postCode')?.value as string),
-            city: this.cityFormGroup.get('city')?.value as string,
-        };
+  performRegistration() {
+    const formData: { password: string; city: string; department: number; email: string; username: string } = {
+      username: this.userNameFormGroup.get('userName')?.value as string,
+      email: this.emailFormGroup.get('email')?.value as string,
+      password: this.passwordFormGroup.get('password')?.value as string,
+      department: parseInt(this.departmentFormGroup.get('department')?.value as string),
+      city: this.cityFormGroup.get('city')?.value as string,
+    };
 
-        this._registerService.register(formData).subscribe({
-            next: (response) => {
-                const successRegistration = this._translate.instant('Toasts.register-sucess');
-                this._alertService.openSnackBar(successRegistration, SnackbarStatus.success);
+    this._registerService.register(formData).subscribe({
+      next: (response) => {
+        const successRegistration = this._translate.instant('Toasts.register-sucess');
+        this._alertService.openSnackBar(successRegistration, SnackbarStatus.success);
 
-                setTimeout(() => {
-                    this._router.navigate(['/login'], {
-                        queryParams: { username: formData.username },
-                    });
-                }, 1200);
-            },
-            error: () => {
-                const errorRegistration = this._translate.instant('Toasts.register-fail');
-                this._alertService.openSnackBar(errorRegistration, SnackbarStatus.error);
-            },
-        });
-    }
+        setTimeout(() => {
+          this._router.navigate(['/login'], {
+            queryParams: {username: formData.username},
+          });
+        }, 1200);
+      },
+      error: () => {
+        const errorRegistration = this._translate.instant('Toasts.register-fail');
+        this._alertService.openSnackBar(errorRegistration, SnackbarStatus.error);
+      },
+    });
+  }
 
-    // validators
+  // validators
 
-    checkUsernameAvailabilityValidator(
-        registerService: RegisterService,
-        snackBar: AlertService
-    ): AsyncValidatorFn {
-        return (control: AbstractControl): Observable<ValidationErrors | null> => {
-            if (!control.value) {
-                return of(null);
-            }
-            return registerService.checkAvailability(control.value, '').pipe(
-                map((response) => {
-                    if (!response.isAvailable) {
-                        const usernameNotAvailable = this._translate.instant(
-                            'Toasts.username-not-available'
-                        );
-                        this._alertService.openSnackBar(usernameNotAvailable, SnackbarStatus.error);
-
-                        return { usernameUnavailable: true };
-                    }
-                    return null;
-                })
+  checkUsernameAvailabilityValidator(
+    registerService: RegisterService,
+    snackBar: AlertService
+  ): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value) {
+        return of(null);
+      }
+      return registerService.checkAvailability(control.value, '').pipe(
+        map((response) => {
+          if (!response.isAvailable) {
+            const usernameNotAvailable = this._translate.instant(
+              'Toasts.username-not-available'
             );
-        };
-    }
+            this._alertService.openSnackBar(usernameNotAvailable, SnackbarStatus.error);
 
-    checkEmailAvailabilityValidator(
-        registerService: RegisterService,
-        snackBar: AlertService
-    ): AsyncValidatorFn {
-        return (control: AbstractControl): Observable<ValidationErrors | null> => {
-            if (!control.value) {
-                return of(null);
-            }
-            return registerService.checkAvailability('', control.value).pipe(
-                map((response) => {
-                    if (!response.isAvailable) {
-                        const emailUnavailable = this._translate.instant(
-                            'Toasts.email-not-available'
-                        );
-                        this._alertService.openSnackBar(emailUnavailable, SnackbarStatus.error);
+            return {usernameUnavailable: true};
+          }
+          return null;
+        })
+      );
+    };
+  }
 
-                        return { emailUnavailable: true };
-                    }
-                    return null;
-                })
+  checkEmailAvailabilityValidator(
+    registerService: RegisterService,
+    snackBar: AlertService
+  ): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value) {
+        return of(null);
+      }
+      return registerService.checkAvailability('', control.value).pipe(
+        map((response) => {
+          if (!response.isAvailable) {
+            const emailUnavailable = this._translate.instant(
+              'Toasts.email-not-available'
             );
-        };
-    }
+            this._alertService.openSnackBar(emailUnavailable, SnackbarStatus.error);
+
+            return {emailUnavailable: true};
+          }
+          return null;
+        })
+      );
+    };
+  }
 }
--- src/app/features/user-panel/profile/pages/profile-page/profile-page.component.ts
+++ src/app/features/user-panel/profile/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,220 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {UserInfoStatesService} from "../../../../../shared/user/services/user-info-states.service";
+import {LoginService} from "../../../../auth/shared/services/login.service";
+import {UserInfosService} from "../../shared/services/user-infos.service";
+import {franceDepartments} from "../../../../auth/shared/departments-utils";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {AlertService} from "../../../../../shared/services/alert.service";
+import {MatDialog} from "@angular/material/dialog";
+import {ModalModifypassword} from "../../modal/modal-modify-password/modal-modify-password.component";
+import {Observable, of} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {SnackbarStatus} from "../../../../../shared/enums/snackbar-status.enum";
+import {RegisterService} from "../../../../auth/shared/services/register.service";
+import {MatCardModule} from "@angular/material/card";
+import {ModalDeleteUser} from "../../modal/modal-delete-user/modal-delete-user.component";
+import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-profile-page',
-    standalone: true,
-    imports: [CommonModule],
-    templateUrl: './profile-page.component.html',
-    styleUrls: ['./profile-page.component.scss'],
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatOptionModule,
+    MatSelectModule,
+    TranslateModule,
+    MatCardModule
+  ],
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent implements OnInit {
+  private _fb = inject(FormBuilder);
+  private _userInfoStatesService = inject(UserInfoStatesService);
+  private _userInfosService = inject(UserInfosService);
+  private _authService = inject(LoginService);
+  private _snackBar = inject(MatSnackBar);
+  private _alertService = inject(AlertService);
+  private _dialog = inject(MatDialog);
+  private _translate = inject(TranslateService);
+  private _registerService = inject(RegisterService);
+  private _router = inject(Router)
+
+
+  userId!: number;
+  departments: any = franceDepartments;
+  initialEmail!: string;
+
+
+  userNameForm = this._fb.group({
+    username: [{value: ''}, [Validators.required, Validators.minLength(3)], this.checkUsernameAvailabilityValidator()]
+  });
+
+  userForm = this._fb.group({
+    email: [{
+      value: '',
+      disabled: false
+    }, [Validators.required, Validators.email], [this.checkEmailAvailabilityValidator()]],
+    department: ['', Validators.required],
+    city: ['', Validators.required],
+    isActive: [],
+  });
+
+  ngOnInit(): void {
+
+
+    this._userInfoStatesService.getUserInfo$().subscribe((userInfo: any) => {
+      if (userInfo) {
+        const departmentCode = Number(userInfo.department);
+        const userDepartment = this.departments.find((department: any) => Number(department.code) === departmentCode);
+
+        this.userForm.patchValue({
+          email: userInfo.email,
+          department: userDepartment ? userDepartment.code : '',
+          city: userInfo.city,
+          isActive: userInfo.isActive
+        });
+        this.userNameForm.patchValue({
+          username: userInfo.username
+        });
+
+        this.userId = userInfo.id;
+        this.initialEmail = userInfo.email;
+      }
+    });
+  }
+
+  openModifyPasswordDialog() {
+    this._dialog.open(ModalModifypassword, {
+      width: '600px'
+    });
+  }
+
+  openDeleteUserDialog() {
+  this._dialog.open(ModalDeleteUser, {
+      width: '600px'
+    });
+  }
+
+  changeUsername() {
+    if (this.userNameForm.valid) {
+      const username: any = this.userNameForm.get('username')?.value;
+      if (username) {
+        const alertInfo = this._translate.instant('UpdateProfile.change-username-info', { username });
+        const alertTitle = this._translate.instant('UpdateProfile.change-username-title');
+
+        console.log('alertInfo:', alertInfo); // Debugging log
+
+        this._alertService.openConfirmDialog(alertTitle, alertInfo).subscribe(result => {
+          if (result) {
+            const updatedUser = { ...this._userInfoStatesService.getUserInfo(), username };
+            this._userInfoStatesService.setUserInfo(updatedUser);
+
+            this._userInfosService.updateUsername(this.userId, username).subscribe({
+              next: () => {
+                const userUpdate = this._translate.instant('UpdateProfile.toast.username');
+                this._alertService.openSnackBar(userUpdate, SnackbarStatus.success);
+              },
+              error: (err: any) => {
+                console.error('Error updating username:', err);
+                this._alertService.openSnackBar('Failed to update username', SnackbarStatus.error);
+              }
+            });
+          }
+        });
+      }
+    }
+  }
+
+
+  deleteAccount() {
+    const alertInfo = this._translate.instant('UpdateProfile.change-username-info');
+    const alerTitle = this._translate.instant('UpdateProfile.change-username-title');
+    this._alertService.openConfirmDialog(alerTitle, alertInfo);
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+
+      const updatedUserInfo = this.userForm.getRawValue();
+      this._userInfosService.updateUser(this.userId, updatedUserInfo).subscribe({
+        next: () => {
+          const userUpdate = this._translate.instant('UpdateProfile.toast.userInfos');
+          this._alertService.openSnackBar(userUpdate, SnackbarStatus.success);
+        },
+        error: (err) => {
+          const userUpdate = this._translate.instant('UpdateProfile.toast.update-fail');
+          this._alertService.openSnackBar(userUpdate, SnackbarStatus.error);
+        }
+      });
+    }
+  }
+
+  checkEmailAvailabilityValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value || control.value === this.initialEmail || !control.dirty) {
+        return of(null);
+      }
+
+      return this._registerService.checkAvailability("", control.value).pipe(
+        map((response: any) => {
+          if (!response.isAvailable) {
+            const emailUnavailable = this._translate.instant('Toasts.email-not-available');
+            this._alertService.openSnackBar(emailUnavailable, SnackbarStatus.error);
+            return {emailUnavailable: true};
+          }
+          return null;
+        })
+      );
+    };
+  }
+
+  checkUsernameAvailabilityValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value || !control.dirty) {
+        return of(null);
+      }
+
+      return this._registerService.checkAvailability(control.value, "").pipe(
+        map((response: any) => {
+          if (!response.isAvailable) {
+            const usernameNotAvailable = this._translate.instant('Toasts.username-not-available');
+            this._alertService.openSnackBar(usernameNotAvailable, SnackbarStatus.error);
+            return {usernameUnavailable: true};
+          }
+          return null;
+        })
+      );
+    };
+  }
+
+
+  isUsernameFormModified(): boolean {
+    return this.userNameForm.dirty;
+  }
+
+  isFormModified(): boolean {
+    return this.userForm.dirty;
+  }
+}
