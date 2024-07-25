afab2e6f9d1b7c1ef51d2ebd6c61b80bb2181b23
Antoine <cassagnea@gmail.com>
2024-07-25 13:47:35 +0200

    Add credentials to endpoints

--- src/app/features/auth/shared/repositories/login.repository.ts
+++ src/app/features/auth/shared/repositories/login.repository.ts
@@ -5,48 +5,49 @@ import { map, Observable } from 'rxjs';
 import { ENVIRONMENT } from '../../../../../backend-endpoints';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class LoginRepository {
-    private readonly http = inject(HttpClient);
-    private readonly router = inject(Router);
+  private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
 
-    private loginUrl = `${ENVIRONMENT.apiLoginConfigurationURL}`;
-    private logoutUrl = `${ENVIRONMENT.apiLogoutConfigurationURL}`;
-    private passwordForgottenUrl = `${ENVIRONMENT.apiPasswordForgottenConfigurationURL}`;
-    private newPasswordUrl = `${ENVIRONMENT.apiNewPasswordConfigurationURL}`;
+  private loginUrl = `${ENVIRONMENT.apiLoginConfigurationURL}`;
+  private logoutUrl = `${ENVIRONMENT.apiLogoutConfigurationURL}`;
+  private passwordForgottenUrl = `${ENVIRONMENT.apiPasswordForgottenConfigurationURL}`;
+  private newPasswordUrl = `${ENVIRONMENT.apiNewPasswordConfigurationURL}`;
 
-    login(credentials: { username: string; password: string }): Observable<boolean> {
-        return this.http
-            .post<any>(this.loginUrl, credentials, {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json',
-                }),
-                withCredentials: true,
-                observe: 'response',
-                params: undefined,
-            })
-            .pipe(
-                map((response) => {
-                    return response.status === 200;
-                })
-            );
-    }
+  login(credentials: { username: string; password: string }): Observable<boolean> {
+    return this.http
+      .post<any>(this.loginUrl, credentials, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+        withCredentials: true,
+        observe: 'response',
+        params: undefined,
+      })
+      .pipe(
+        map((response) => {
+          return response.status === 200;
+        })
+      );
+  }
 
-    logout(): Observable<any> {
-        return this.http.post(this.logoutUrl, { withCredentials: true });
-    }
+  logout(): Observable<any> {
+    return this.http.post(this.logoutUrl, {}, { withCredentials: true });
+  }
 
-    requestPasswordReset(email: string): Observable<any> {
-        return this.http.post(`${this.passwordForgottenUrl}/${email}`, {});
-    }
+  requestPasswordReset(email: string): Observable<any> {
+    return this.http.post(`${this.passwordForgottenUrl}/${email}`, {}, { withCredentials: true });
+  }
 
-    resetPassword(
-        token: string,
-        newPasswordData: { plainPassword: string; plainPasswordVerification: string }
-    ): Observable<any> {
-        return this.http.post(`${this.newPasswordUrl}/${token}`, newPasswordData, {
-            headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-        });
-    }
+  resetPassword(
+    token: string,
+    newPasswordData: { plainPassword: string; plainPasswordVerification: string }
+  ): Observable<any> {
+    return this.http.post(`${this.newPasswordUrl}/${token}`, newPasswordData, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    });
+  }
 }
